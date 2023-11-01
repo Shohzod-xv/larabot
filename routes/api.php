fc9b1a60674b3f8987a55b89aaecc1548588829a
Shohzod Xayrullayev <usermac@192.168.0.129>
2023-11-01 16:08:01 +0500

    sjkdznv

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::any('/{key}', [TelegramController::class, 'index']);
+Route::any('/{key}', [TelegramController::class, 'index'])->name('webhook');
 
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/webhook', [TelegramController::class, 'webhook'])->name('webhook');
+Route::get('/webhook', [TelegramController::class, 'webhook']);
